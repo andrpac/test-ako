0ae0336e55493d2405bdd2db7ae1577975f2fd8c
andrpac <andrei.pacurar@mongodb.com>
2025-08-02 16:18:12 +0100

    new-fix

--- scripts/release-pr.sh
+++ scripts/release-pr.sh
@@ -54,6 +54,7 @@ echo "Branch pushed to remote"
 if [[ -d "$RELEASE_DIR" ]]; then
   echo "Cleaning up existing release dir: $RELEASE_DIR"
   git rm -rf "$RELEASE_DIR"
+  git add .
   export COMMIT_MESSAGE="chore: remove ${RELEASE_DIR} to prepare fresh release"
   scripts/create-signed-commit.sh
 fi
