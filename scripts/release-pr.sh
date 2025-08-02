311c00b06ae966a99e8779b6130f8a1fa9a09945
andrpac <andrei.pacurar@mongodb.com>
2025-08-02 13:12:09 +0100

    new-fix

--- scripts/release-pr.sh
+++ scripts/release-pr.sh
@@ -46,12 +46,8 @@ cp -r helm-charts "$TMP_DIR/helm-charts"
 cp bundle.Dockerfile "$TMP_DIR/bundle.Dockerfile"
 
 git fetch origin
-
-if git rev-parse --verify "$BRANCH" >/dev/null 2>&1; then
-  git branch -D "$BRANCH"
-fi
-
 git checkout -B "$BRANCH" origin/main
+git push -f origin
 
 if [[ -d "$RELEASE_DIR" ]]; then
   echo "Cleaning up existing release dir: $RELEASE_DIR"
